cb79ced1f690a63a27f8a07a5dd831b158add6a3
Roxanne Krause <rlutz1@unm.edu>
2025-12-22 16:27:02 -0700

    pushing little things, nothing major, in planning stages

--- run.py
+++ run.py
@@ -1,16 +1,26 @@
-from PyQt5.QtWidgets import QApplication, QWidget
-
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow
+from PyQt5.QtCore import QSize, Qt
 # Only needed for access to command line arguments
 import sys
 
+# https://www.pythonguis.com/tutorials/creating-your-first-pyqt-window/
+
 # You need one (and only one) QApplication instance per application.
 # Pass in sys.argv to allow command line arguments for your app.
-# If you know you won't use command line arguments QApplication([]) works too.
-app = QApplication(sys.argv)
+# app = QApplication(sys.argv)
+app = QApplication([]) # empty for now, use above if command args become a thing
 
 # Create a Qt widget, which will be our window.
-window = QWidget()
+# window = QWidget()
+window = QMainWindow()
+window.setWindowTitle("Graph Algorithm Visualizer") # basic title
+window.setMinimumSize(QSize(1000, 500))
+
+button = QPushButton("Gimme a sexy push")
+button.setFixedSize(QSize(200, 50))
+window.setCentralWidget(button) # set the central widget in vanilla layout of main winodw
 window.show()  # IMPORTANT!!!!! Windows are hidden by default.
+
 
 # Start the event loop.
 app.exec()
