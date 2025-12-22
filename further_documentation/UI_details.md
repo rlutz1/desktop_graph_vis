ed37dd0b717c7eb17f2aab6ebf45278f0d5a2a75
Roxanne Krause <rlutz1@unm.edu>
2025-12-22 15:26:27 -0700

    adding potential of using pyqt5 instead since tkinter is probably a bad idea for any sort of animating

--- further_documentation/UI_details.md
+++ further_documentation/UI_details.md
@@ -8,4 +8,9 @@ Any and all notes on the technical details of the UI can be read as desired belo
 
 Current rendition of this application will simply use Tkinter as an easier way to build out a front end. There is an eye for something better (PyQT5 considered) or for replacing with a website, however this will work for some general ideas first.
 
-<a href = "https://github.com/rdbende/Forest-ttk-theme">This theme</a> is used on top of Tkinter for a more modern feel. Much love to those devs. 
+<a href = "https://github.com/rdbende/Forest-ttk-theme">This theme</a> is used on top of Tkinter for a more modern feel. Much love to those devs.
+
+### Dev Notes
+
++ Likely Tkinter will be limiting in terms of "animation" and how smooth that could potentially be. But maybe could work for super basic work. 
++ Looked a bit into <a href="https://3b1b.github.io/manim/getting_started/quickstart.html">manim</a>. Something that could potentially be useful.
--- run.py
+++ run.py
@@ -1,16 +1,49 @@
-import tkinter as tk
-from tkinter import ttk
+from PyQt5.QtWidgets import QApplication, QWidget
 
-root = tk.Tk()
+# Only needed for access to command line arguments
+import sys
 
-# Import the tcl file
-root.tk.call('source', 'UI/tkinter_theme/forest-dark.tcl')
+# You need one (and only one) QApplication instance per application.
+# Pass in sys.argv to allow command line arguments for your app.
+# If you know you won't use command line arguments QApplication([]) works too.
+app = QApplication(sys.argv)
 
-# Set the theme with the theme_use method
-ttk.Style().theme_use('forest-dark')
+# Create a Qt widget, which will be our window.
+window = QWidget()
+window.show()  # IMPORTANT!!!!! Windows are hidden by default.
 
-# A themed (ttk) button
-button = ttk.Button(root, text="I'm a themed button")
-button.pack(pady=20)
+# Start the event loop.
+app.exec()
 
-root.mainloop()
+
+# Your application won't reach here until you exit and the event
+# loop has stopped.
+
+# ======================================================
+# tkinter window below
+# ======================================================
+
+# import tkinter as tk
+# from tkinter import ttk
+
+# root = tk.Tk()
+
+# # Import the tcl file
+# root.tk.call('source', 'UI/tkinter_theme/forest-dark.tcl')
+
+# # Set the theme with the theme_use method
+# ttk.Style().theme_use('forest-dark')
+
+# # set some meta about the application
+# # https://tkdocs.com/pyref/tk.html
+# root.title("Graph Algorithm Visualizer")
+# # root.resizable(width=False, height=False)
+# root.minsize(width=1000, height=500)
+# mainframe = ttk.Frame(root, padding=(3, 3, 12, 12), width=500, height=500)
+# # mainframe.grid(column=0, row=0)
+
+# # A themed (ttk) button
+# button = ttk.Button(root, text="I'm a themed button")
+# button.pack(pady=20)
+
+# root.mainloop()
