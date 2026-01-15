45832263814c61c2f33f6f57ac43e0966cd7f103
Roxanne Krause <rlutz1@unm.edu>
2026-01-15 08:04:14 -0700

    adding some very generic definitions of some high level backend constructs for work flow, including the visual, updater.

--- Backend_Components/algorithm.py
+++ Backend_Components/algorithm.py
@@ -28,8 +28,8 @@ class Algorithm:
 
 class State:
 
-  __action = None # the action this state will carry out tangibly on backend structures
-  __visual = None # the visual this state will carry out on the front end as a visualization
+  _action = None # the action this state will carry out tangibly on backend structures
+  _visual = None # the visual this state will carry out on the front end as a visualization
 
   def __init__(self, action, visual):
     if action is None:
@@ -38,7 +38,62 @@ class State:
     if visual is None:
       print("No visual assigned to this state--are you sure you want that?")
     
-    self.__action = action
-    self.__visual = visual
+    self._action = action
+    self._visual = visual
     print("State initialized.")
+
+
+# ============================================================
+# we're gonna focus for a moment on the visualization here.
+# a visual is essentially just a mapping of ID's that identify
+# uniquely a changeable component on the front end to an 
+# Updater. this visual is to be given to the interpretation layer
+# between the back and front end so it can hand off appropriately to 
+# the visuals.
+# ============================================================
+
+class Visual():
+
+  _visuals = {}
+
+  def __init__(self):
+    pass
+
+  # add an updater for this widget on the front end.
+  # one rule right now: only one updater allowed
+  # to keep things simpler.
+  def add_visual(self, id, updater):
+    self._visuals[id] = updater
+
+  def visuals(self):
+    return self._visuals
+  
+  def ids(self):
+    return self._visuals.keys()
+  
+
+# ============================================================
+# let's talk updater. an updaters job is simply to have an 
+# update function that says how to visually change the parent.
+# it is up to the developers capability to know what the 
+# widget being changed is and specify accordingly.
+# this is handed to the front end with the id, which is then taken, 
+# the parent is assigned to the front end and then "installed"
+# in the front end widget to call .update() on a paint event occurrence.
+# ============================================================
+
+class Updater():
+
+  _widget = None # the front end widget to change
+
+  def __init__(self):
+    pass
+
+  # this the method that should always be overriden 
+  # by a child updater.
+  def update(self):
+    pass
+
+  def assign_parent(self, widget):
+    self._widget = widget
 
