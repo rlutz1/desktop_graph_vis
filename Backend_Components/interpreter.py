eb14688773083ceb38542ca24cf98f4f8e7673f9
Roxanne Krause <rlutz1@unm.edu>
2026-01-15 10:05:49 -0700

    adding an access point A -> V in interp

--- Backend_Components/interpreter.py
+++ Backend_Components/interpreter.py
@@ -9,5 +9,23 @@
 
 class Interpreter():
 
-  def __init__():
-    pass
+  _algorithm = None
+  _visualizer = None
+
+  def __init__(self, algorithm, visualizer):
+    self._algorithm = algorithm
+    self._visualizer = visualizer
+
+  # expecting a Visual as sent from the algorithm
+  # to give to the visualizer in pieces
+  def send_visual(self, visuals):
+    for id in visuals.ids():
+      # send the visualizer the updater along with unique id
+      self._visualizer.update_visual(id, visuals[id]) # TODO
+    self._visualizer.update() # unsure if this is needed yet or if appropriate
+    # i think i need to only update the widgets with changes, not sure if 
+    # the paint event "falls" down through the inheritance tree
+  
+  # notify the algo we are finished. unclear if needed yet.
+  # def notify_complete(self):
+  #    self._algorithm.notify()
